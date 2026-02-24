98d2eccfc333cb2329ae093d12bce9b983034407
Aditya Kurne <137620112+EMPER0R7@users.noreply.github.com>
2026-02-24 19:20:54 +0530

    track event fix

--- src/routes/recommend.routes.js
+++ src/routes/recommend.routes.js
@@ -7,6 +7,6 @@ const { getContentDetails } = require("../controllers/content.controller");
 router.get("/content/:type/:id", getContentDetails);
 
 router.post("/", recommend);
-router.post("/event", trackEvent);
+// router.post("/event", trackEvent);
 
 module.exports = router;
